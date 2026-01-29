f4f5dfb42895216ad50262c798a7c40f8fb73188
Robert Jensen <robert@robert-jensen.dk>
2026-01-29 10:25:44 +0100

    change url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://rebuy.today',
+  site: 'https://incubatordk.github.io',
+  base: '/rebuy.today/',
   output: 'static',
 });
